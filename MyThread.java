49d5cbc8593c36bedcb3c4857d11107d2ecdace4
ApurvaJatala <97779496+ApurvaJatala@users.noreply.github.com>
2022-01-15 19:48:15 +0530

    Add files via upload

---
+++ MyThread.java
@@ -0,0 +1,33 @@
+package apu;
+
+public class MyThread extends Thread
+{
+	public void run()
+		{  
+			for (int i = 0; i < 3; i++)
+			{
+		   System.out.println("running thread name is:"+Thread.currentThread().getName());  // name of the thread
+		   System.out.println("running thread state is:"+ Thread.currentThread().getName()+ " :::" + Thread.currentThread().getState());  // it will print state of the thread running
+		   System.out.println("The thread id is:"+ Thread.currentThread().getName()+ " :::" + Thread.currentThread().getId()); // CPU scheduler has given a unique ID to each thread
+		   System.out.println("Is my thread alive or not? :  "+ Thread.currentThread().getName()+ " :::" + Thread.currentThread().isAlive());  // it will heck if thread is alive or dead
+			}  
+		}
+		
+		 public static void main(String args[])
+		 {  
+		  
+			 MyThread m1=new MyThread();  
+			 MyThread m2=new MyThread();  
+			 MyThread m3=new MyThread();
+		  
+		  m1.setName("IT");
+		  m2.setName("Finance");
+		  m3.setName("HR");
+		 
+		  
+		  m1.start();  
+		  m2.start();  
+		  m3.start();
+		   
+		 }  
+}
